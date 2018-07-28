2713f804eca695798264a89fe4fdf5a27f4b5832
Claudio P. Bandera <cbandera@posteo.de>
2018-07-28 11:52:59 +0200

    Refactoring

--- GarageStopLight.ino
+++ GarageStopLight.ino
@@ -1,21 +1,28 @@
 #include <LowPower.h>
 #include <NewPing.h>
 
-#define WIP 1
-#define TRIGGER_PIN 11
-#define ECHO_PIN 12
+/// Behavioral Options
+#define USE_SERIAL 0
+#define visualizeFct visualizeAsLightbar // visualizeAsStrobe
+#define SLEEP_DURATION SLEEP_4S
 #define MIN_DISTANCE 10
 #define MAX_DISTANCE 100
-#define MIN_FREQUENCY 1
-#define MAX_FREQUENCY 20
+#define MIN_STROBE_FREQUENCY 1
+#define MAX_STROBE_FREQUENCY 20
+#define INACTIVITY_LIMIT 50
+
+/// Pin Setup
+#define TRIGGER_PIN 11
+#define ECHO_PIN 12
 #define LED_PIN_MIN 2 // RED (PS: Don't use Serial ports)
 #define LED_PIN_MAX 9 // GREEN
-#define INACTIVITY_LIMIT 20
 
+/// Constants
 #define LED_COUNT (LED_PIN_MAX - LED_PIN_MIN + 1) // Inclusive the LED_PIN_MAX
 #define DS ((MAX_DISTANCE - MIN_DISTANCE) / LED_COUNT)
 #define INVALID_DISTANCE 0
 
+/// Globals
 NewPing
     g_sonarSensor(TRIGGER_PIN, ECHO_PIN,
                   MAX_DISTANCE); // NewPing setup of pins and maximum distance.
@@ -25,39 +32,39 @@ int g_lastMeasurement = 0;
 
 int indexToPin(int index) { return LED_PIN_MIN + index; }
 
-void setLedStates(bool state, int upto = LED_COUNT) {
+void setAllLedStates(bool state, int upto = LED_COUNT) {
   upto = min(upto, LED_COUNT);
   for (int i = 0; i < upto; ++i) {
     g_ledStates[i] = state;
   }
 }
 
-void writeLedPins() {
+void writeLedStatesToPins() {
   for (int i = 0; i < LED_COUNT; ++i) {
     digitalWrite(indexToPin(i), g_ledStates[i]);
   }
 }
 
-void blink(int frequency = 10) {
-  setLedStates(LOW);
-  writeLedPins();
+void blinkLeds(int frequency = 10) {
+  setAllLedStates(LOW);
+  writeLedStatesToPins();
   delay(1000 / frequency);
-  setLedStates(HIGH);
-  writeLedPins();
+  setAllLedStates(HIGH);
+  writeLedStatesToPins();
   delay(1000 / frequency);
 }
 
-void roll(int frequency = 15) {
+void rollLeds(int frequency = 15) {
   for (int i = LED_COUNT - 1; i >= 0; --i) {
-    setLedStates(LOW);
+    setAllLedStates(LOW);
     g_ledStates[i] = HIGH;
-    writeLedPins();
+    writeLedStatesToPins();
     delay(1000 / frequency);
   }
   for (int i = 0; i < LED_COUNT; ++i) {
-    setLedStates(LOW);
+    setAllLedStates(LOW);
     g_ledStates[i] = HIGH;
-    writeLedPins();
+    writeLedStatesToPins();
     delay(1000 / frequency);
   }
 }
@@ -68,16 +75,16 @@ void setupLedPins() {
 }
 
 void setupSerial() {
-#ifdef WIP
+#ifdef USE_SERIAL
   Serial.begin(9600);
 #endif
 }
 void setup() {
   setupSerial();
   setupLedPins();
-  setLedStates(LOW);
-  writeLedPins();
-  roll();
+  setAllLedStates(LOW);
+  writeLedStatesToPins();
+  rollLeds();
 }
 
 int getDistanceInCm() {
@@ -93,65 +100,76 @@ void catchInvalidMeasurement(int &cm) {
 }
 
 void visualizeAsLightbar(const int cm) {
-  setLedStates(HIGH);
+  setAllLedStates(HIGH);
   if (cm > MIN_DISTANCE) {
-    int numLedsOff = (cm - MIN_DISTANCE) / DS;
-    setLedStates(LOW, numLedsOff);
-    writeLedPins();
+    int uptoLedNr = (cm - MIN_DISTANCE) / DS;
+    setAllLedStates(LOW, uptoLedNr);
+    writeLedStatesToPins();
   } else {
-    blink(20);
+    blinkLeds(20);
   }
 }
 
 void visualizeAsStrobe(const int cm) {
-
   if (cm > MIN_DISTANCE) {
     auto factor = (cm - MIN_DISTANCE) / (MAX_DISTANCE - MIN_DISTANCE);
-    int frequency = MAX_FREQUENCY - (MAX_FREQUENCY - MIN_FREQUENCY) * factor;
-    blink(frequency);
+    int frequency = MAX_STROBE_FREQUENCY -
+                    (MAX_STROBE_FREQUENCY - MIN_STROBE_FREQUENCY) * factor;
+    blinkLeds(frequency);
   } else {
-    setLedStates(HIGH);
-    writeLedPins();
+    setAllLedStates(HIGH);
+    writeLedStatesToPins();
   }
 }
 
 void goToSleep() {
-#ifdef WIP
+#ifdef USE_SERIAL
   Serial.println("Going to sleep.");
   Serial.flush();
 #endif
-  blink(15);
-  setLedStates(LOW);
-  writeLedPins();
-  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
-  // LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
+  setAllLedStates(LOW);
+  writeLedStatesToPins();
+  LowPower.powerDown(SLEEP_DURATION, ADC_OFF, BOD_OFF);
+  // LowPower.idle(SLEEP_DURATION, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
   // SPI_OFF, USART0_OFF, TWI_OFF);
 }
 
 void countInactivity(int distance) {
-  if (distance == g_lastMeasurement) {
+  if (abs(distance - g_lastMeasurement) <= 1) {
     ++g_inactivityCount;
   } else {
     g_inactivityCount = 0;
+    g_lastMeasurement = distance;
   }
-  g_lastMeasurement = distance;
 }
 
-void sleepUponInactivity() {
+bool sleepUponInactivity() {
   if (g_inactivityCount >= INACTIVITY_LIMIT) {
     // Do not reset counter, so that we go back to sleep quickly if measurement
     // stays the same
     goToSleep();
+    return true;
   }
+  return false;
 }
 void loop() {
   int distance = getDistanceInCm();
   catchInvalidMeasurement(distance);
   countInactivity(distance);
-  sleepUponInactivity();
-  visualizeAsLightbar(distance);
-  // visualizeAsStrobe(distance);
-#ifdef WIP
-  Serial.println(distance);
+#ifdef USE_SERIAL
+  Serial.print(distance);
+  Serial.print(" ");
+  Serial.print(MIN_DISTANCE);
+  Serial.print(" ");
+  Serial.print(MAX_DISTANCE);
+  Serial.print(" ");
+  Serial.print(g_inactivityCount);
+  Serial.print(" ");
+  Serial.println(INACTIVITY_LIMIT);
+  
 #endif
+  if (sleepUponInactivity())
+    return; // aka continue
+  else
+    visualizeFct(distance);
 }
